b8d2ab49e00c494d6821854b3f281651c6f33ffa
Tony <king8sky@gmail.com>
2024-08-04 13:24:36 +1100

    adding a subscription model for course updates

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import CourseViewSet, LessonList, LessonCreate, LessonRetrieve, LessonUpdate, LessonDelete
+from materials.views import CourseViewSet, LessonList, LessonCreate, LessonRetrieve, LessonUpdate, LessonDelete, \
+    SubscriptionAPIView
 
 app_name = MaterialsConfig.name
 
@@ -10,9 +11,10 @@ router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='material')
 
 urlpatterns = [
-    path('lesson/', LessonList.as_view(), name='lesson_list'),
-    path('lesson/create', LessonCreate.as_view(), name='lesson_create'),
-    path('lesson/<int:pk>', LessonRetrieve.as_view(), name='lesson_get'),
-    path('lesson/update/<int:pk>', LessonUpdate.as_view(), name='lesson_update'),
-    path('lesson/delete/<int:pk>', LessonDelete.as_view(), name='lesson_delete'),
-] + router.urls
+                  path('lesson/', LessonList.as_view(), name='lesson_list'),
+                  path('lesson/create', LessonCreate.as_view(), name='lesson_create'),
+                  path('lesson/<int:pk>', LessonRetrieve.as_view(), name='lesson_get'),
+                  path('lesson/update/<int:pk>', LessonUpdate.as_view(), name='lesson_update'),
+                  path('lesson/delete/<int:pk>', LessonDelete.as_view(), name='lesson_delete'),
+                  path('subscription/', SubscriptionAPIView.as_view(), name='subscription_create')
+              ] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -1,14 +1,19 @@
 from rest_framework import viewsets, generics
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from materials.models import Course, Lesson
-from materials.serializers import CourseSerializer, LessonSerializer
+from materials.models import Course, Lesson, Subscription
+from materials.paginators import CustomPagination
+from materials.serializers import CourseSerializer, LessonSerializer, SubscriptionSerializer
 from users.permissions import IsModerator, IsOwner
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    pagination_class = CustomPagination
 
     def get_permissions(self):
         if self.action == "create":
@@ -38,6 +43,7 @@ class LessonCreate(generics.CreateAPIView):
 class LessonList(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    pagination_class = CustomPagination
 
 
 class LessonRetrieve(generics.RetrieveAPIView):
@@ -55,3 +61,23 @@ class LessonUpdate(generics.UpdateAPIView):
 class LessonDelete(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     permission_classes = (IsAuthenticated, IsOwner | ~IsModerator,)
+
+
+class SubscriptionAPIView(APIView):
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner | ~IsModerator,)
+
+    def post(self, request):
+        user = self.request.user
+        course_id = self.request.data.get('course')
+        course = get_object_or_404(Course, pk=course_id)
+
+        subs_item = Subscription.objects.filter(user=user, course=course)
+        if subs_item.exists():
+            subs_item.delete()
+            message = 'Подписка удалена'
+        else:
+            Subscription.objects.create(user=user, course=course)
+            message = 'Подписка добавлена'
+        return Response({"message": message})
